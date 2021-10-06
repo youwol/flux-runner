8af6dfe1774bba01a522d281a642345831ee946b
greinisch <greinisch@youwol.com>
2021-10-06 17:36:32 +0200

    :zap: fetch styles & bundles in //

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,25 +3,26 @@
 // (index.html is handled by HtmlWebpackPlugin)
 
 import { fetchBundles, fetchStyleSheets } from '@youwol/cdn-client';
-export{}
+export {}
 
 require('./style.css');
 
-await fetchStyleSheets([
-    "bootstrap#4.4.1~bootstrap.min.css",
-    "fontawesome#5.12.1~css/all.min.css",
-    "@youwol/fv-widgets#0.0.3~dist/assets/styles/style.youwol.css"
+await Promise.all([
+    fetchStyleSheets([
+        "bootstrap#4.4.1~bootstrap.min.css",
+        "fontawesome#5.12.1~css/all.min.css",
+        "@youwol/fv-widgets#0.0.3~dist/assets/styles/style.youwol.css"
+    ]),
+    fetchBundles({
+        'lodash': '4.17.15',
+        "grapes": '0.16.2',
+        "@youwol/flux-core": 'latest',
+        '@youwol/flux-view': 'latest',
+        "rxjs": '6.5.5',
+        },
+        window
+    )
 ])
-
-await fetchBundles({
-    'lodash': '4.17.15',
-    "grapes": '0.16.2',
-    "@youwol/flux-core": 'latest',
-    '@youwol/flux-view': 'latest',
-    "rxjs": '6.5.5',
-    },
-    window
-)
 
 await import('./on-load')
 
