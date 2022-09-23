03d06bf2083ead295535bd0d539cbd3fd76d543f
youwol-guillaume <greinisch@youwol.com>
2022-09-23 09:31:27 +0200

    :recycle: main.ts => use setup.installMainModule

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,26 +1,26 @@
 require('./style.css')
 import { setup } from '../auto-generated'
-import { install, LoadingScreenView } from '@youwol/cdn-client'
+import * as cdnClient from '@youwol/cdn-client'
 export {}
 
-const loadingScreen = new LoadingScreenView({
+const loadingScreen = new cdnClient.LoadingScreenView({
     container: document.body,
 })
 loadingScreen.render()
 // this variable will be used in the on-load.ts to continue displaying dependencies fetching
 window['fluRunnerLoadingScreen'] = loadingScreen
 
-await install({
-    modules: Object.entries(setup.runTimeDependencies.load).map(
-        ([k, v]) => `${k}#${v}`,
-    ),
-    css: [
-        'bootstrap#4.4.1~bootstrap.min.css',
-        'fontawesome#5.12.1~css/all.min.css',
-        '@youwol/fv-widgets#0.0.3~dist/assets/styles/style.youwol.css',
-    ],
-    onEvent: (event) => {
-        loadingScreen.next(event)
+await setup.installMainModule({
+    cdnClient,
+    installParameters: {
+        css: [
+            'bootstrap#4.4.1~bootstrap.min.css',
+            'fontawesome#5.12.1~css/all.min.css',
+            '@youwol/fv-widgets#0.0.3~dist/assets/styles/style.youwol.css',
+        ],
+        onEvent: (event) => {
+            loadingScreen.next(event)
+        },
     },
 })
 
