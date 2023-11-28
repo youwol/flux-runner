a8e704e8c9bd6e45b1e62df08026c39fa91f53b0
jehadmelad <jd.melad@gmail.com>
2023-11-28 16:11:03 +0100

    ✨  yw_pipeline.py => add `PublishConfig` argument to enable assets feature (i.e. `packagedFolders=["assets"]`)

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,7 +1,7 @@
 from youwol.app.environment import YouwolEnvironment
 from youwol.app.environment.models import IPipelineFactory
 from youwol.app.environment.models_project import BrowserApp, Execution, OpenWith, BrowserAppGraphics, Link
-from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, PublishConfig
 from youwol.utils.context import Context
 
 
@@ -41,6 +41,9 @@ class PipelineFactory(IPipelineFactory):
                         }
                     }
                 ),
+            ),
+            publishConfig=PublishConfig(
+                packagedFolders=["assets"],
             )
         )
         return await pipeline(config, context)
